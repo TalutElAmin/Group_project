4dba27a6bb76636dd548c0555ccaab57aebd623a
TalutElAmin <talutaminu@gmail.com>
2023-04-27 05:40:15 -0500

    Adjustment

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
-const menuebar= document.getElementsByClassName
+const menuebar= document.getElementsByClassName("menu")
+const navbar = document.getElementsByClassName("link_bar")
+menuebar.addEventListener("click", ()=>{
+    navbar.classList.toggle('active')
+})
